214a9725f627018532f32cbd3d0bbe5fd3654f54
wendybarrantes <wenbarrantes06@gmail.com>
2024-11-11 12:37:32 -0600

    Actualizacion de los modelos

--- B2BBackEnd/empresas/models.py
+++ B2BBackEnd/empresas/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Empresa(models.Model):
     ...
+    
+    
+class Areas_Trabajo(models.Model):
+    ...
--- B2BBackEnd/encuestas/models.py
+++ B2BBackEnd/encuestas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+
 # Create your models here.
+class tipo_de_encuestas(models.Model):
+    ...
+    
+    
+class preguntas(models.Model):
+    ...
