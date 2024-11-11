64cb976fa56db88b51a8767809b63dbebc5becbc
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-11-11 15:24:39 -0600

    Creacion de los modelos, urls, serializers y las vistas

--- B2BBackEnd/encuestas/models.py
+++ B2BBackEnd/encuestas/models.py
@@ -3,9 +3,16 @@ from django.db import models
 
 
 # Create your models here.
-class tipo_de_encuestas(models.Model):
-    ...
+class Encuestas(models.Model):
+    descripcion = models.TextField()
+    fecha = models.DateField( auto_now= True)
+    preguntas = models.ForeignKey("Preguntas.encuentas", on_delete=models.CASCADE)
+    Tipo_de_encuestas = models.ForeignKey("Tipo_de_encuentas.encuentas", on_delete=models.CASCADE)
     
     
-class preguntas(models.Model):
-    ...
+class Preguntas(models.Model):
+    preguntas = models.CharField(max_length=500)
+    
+    
+class Tipo_de_encuestas(models.Model):
+    tipo = models.CharField(max_length=400)
