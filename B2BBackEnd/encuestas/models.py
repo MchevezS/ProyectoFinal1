72e3674d5952636c2d504e9e205dbbd980358978
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-11-11 15:31:50 -0600

    cambio en los modelos

--- B2BBackEnd/encuestas/models.py
+++ B2BBackEnd/encuestas/models.py
@@ -9,10 +9,17 @@ class Encuestas(models.Model):
     preguntas = models.ForeignKey("Preguntas.encuentas", on_delete=models.CASCADE)
     Tipo_de_encuestas = models.ForeignKey("Tipo_de_encuentas.encuentas", on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.descripcion
     
 class Preguntas(models.Model):
     preguntas = models.CharField(max_length=500)
     
+    def __str__(self):
+        return self.preguntas
     
 class Tipo_de_encuestas(models.Model):
     tipo = models.CharField(max_length=400)
+    
+    def __str__(self):
+        return self.tipo
