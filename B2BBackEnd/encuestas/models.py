2d85b2a41b8752fb3f9ab20b641c10fa3abee309
wendybarrantes <wenbarrantes06@gmail.com>
2024-11-11 15:39:34 -0600

    models(usuarios/empresa)

--- B2BBackEnd/encuestas/models.py
+++ B2BBackEnd/encuestas/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 
-
+class Encuesta(models.Model):
+    ...
 # Create your models here.
-class tipo_de_encuestas(models.Model):
+
+
+
+
+class Tipo_de_encuestas(models.Model):
     ...
+
+
+
+
+class Preguntas(models.Model):
+   
     
     
-class preguntas(models.Model):
-    ...
+    
