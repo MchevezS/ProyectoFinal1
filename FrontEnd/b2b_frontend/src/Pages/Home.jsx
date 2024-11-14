b1e13b2ff21155f2c1125b2942278d063b8bb82d
nazyri <168209590+nazyri@users.noreply.github.com>
2024-11-14 15:40:32 -0600

    Componente Caracteristicas Editar el navbar y los testimonios

--- FrontEnd/b2b_frontend/src/Pages/Home.jsx
+++ FrontEnd/b2b_frontend/src/Pages/Home.jsx
@@ -1,10 +1,15 @@
-
-import Testimonio1 from "../Components/Testimonio1"
+import CaracteristicasPrincipales from "../Components/CaracteristicasPrincipales";
+import Testimonio1 from "../Components/Testimonio1";
+import Testimonio2 from '../Components/Testimonio2';
+import Testimonio3 from '../Components/Testimonio3';
 
 function Home() {
   return (
     <div>
+      <CaracteristicasPrincipales/>
       <Testimonio1/>
+      <Testimonio2/>
+      <Testimonio3/>
     </div>
   )
 }
