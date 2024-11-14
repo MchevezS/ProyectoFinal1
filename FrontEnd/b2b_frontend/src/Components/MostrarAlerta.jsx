d76454d2566b47e0368b565b460ed6f26520d5c2
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-11-14 13:47:39 -0600

    Cambios en las urls, settings, views, en el crud, formRegister, Mostrar Alertas

---
+++ FrontEnd/b2b_frontend/src/Components/MostrarAlerta.jsx
@@ -0,0 +1,24 @@
+import Swal from 'sweetalert2'
+import '/src/style/MostrarAlerta.css'
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'center',
+    iconColor: 'white',
+    customClass: {
+      popup: 'colored-toast',
+    },
+    showConfirmButton: false,
+    timer: 5000,
+    timerProgressBar: true,
+})
+
+const mostrarAlerta = async (icon, title)=>{
+    await Toast.fire({
+        icon: icon,
+        title: title,
+    })
+}
+export {mostrarAlerta}
+
+
