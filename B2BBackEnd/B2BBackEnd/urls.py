e0220678969b06930ed146c186650f46edb0aad7
wendy barrantes <wenbarrantes06@gmail.com>
2024-11-18 01:25:31 -0600

    ENCUESTAS

--- B2BBackEnd/B2BBackEnd/urls.py
+++ B2BBackEnd/B2BBackEnd/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("usuarios.urls"))
+    path("api/", include("usuarios.urls")),
+    path('api/', include("encuestas.urls"))
+    
 ]
