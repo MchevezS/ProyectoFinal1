71c112967b003cc58ae4a985fc26656eeeda46d9
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-11-20 09:02:50 -0600

    CAMBIOS MICHELLE

--- FrontEnd/b2b_frontend/src/Pages/Home.jsx
+++ FrontEnd/b2b_frontend/src/Pages/Home.jsx
@@ -1,9 +1,6 @@
 import Navbar from "../Components/Navbar"
 import CaracteristicasPrincipales from "../Components/CaracteristicasPrincipales";
-<<<<<<< HEAD
-=======
 import FormEmpresas from "../Components/FormEmpresas";
->>>>>>> 6606780ae6c001651298908a8884a3055c295985
 import Testimonio1 from "../Components/Testimonio1";
 import Testimonio2 from '../Components/Testimonio2';
 import Testimonio3 from '../Components/Testimonio3';
@@ -16,10 +13,7 @@ function Home() {
       <Testimonio1/>
       <Testimonio2/>
       <Testimonio3/>
-<<<<<<< HEAD
-=======
       <FormEmpresas/>
->>>>>>> 6606780ae6c001651298908a8884a3055c295985
     </div>
   )
 }
