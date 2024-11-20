a70f7795cc2a6c2d7753d26d9b682a13b2165efe
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-11-20 08:43:57 -0600

    urls

--- B2BBackEnd/B2BBackEnd/urls.py
+++ B2BBackEnd/B2BBackEnd/urls.py
@@ -3,10 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<<<< Temporary merge branch 1
-    path("api/", include('usuarios.urls'))
-]
-=========
+
+    path("api/", include('usuarios.urls')),
+
     path("api/", include("usuarios.urls")),
     path("api/", include("empresas.urls")),
     path('api/', include("encuestas.urls"))
