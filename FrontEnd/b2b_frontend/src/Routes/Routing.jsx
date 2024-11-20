f4097be4fb187ac8f1db4542219a93c8b172098b
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-11-20 16:07:07 -0600

    Cambios michelle

--- FrontEnd/b2b_frontend/src/Routes/Routing.jsx
+++ FrontEnd/b2b_frontend/src/Routes/Routing.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home';
 import Encuestas from '../Pages/Encuestas';
-<<<<<<< HEAD
-=======
 import Empresas from '../Pages/Empresas';
 
->>>>>>> 60fa5bb788f3d34a85790c2e1e0e2debe5b02560
 import Login from '../Pages/Login';
 const Routing = () => {
     return (
@@ -13,12 +10,9 @@ const Routing = () => {
        <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/encuestas' element={<Encuestas/>}/>
-<<<<<<< HEAD
           <Route path='/registroLogin' element={<Login/>}/>
-=======
           <Route path='/empresas' element={<Empresas/>}/>
           <Route path='/login' element={<Login/>}/>
->>>>>>> 60fa5bb788f3d34a85790c2e1e0e2debe5b02560
        </Routes>
       </Router>
      );
