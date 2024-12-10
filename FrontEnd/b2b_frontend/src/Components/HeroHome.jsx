8278dab24b0a5d5694433d8e67b6e8031aee5b41
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-12-10 15:00:28 -0600

    se le dio estilos al hero, y al carousel, las card resenas faltan de estilos

--- FrontEnd/b2b_frontend/src/Components/HeroHome.jsx
+++ FrontEnd/b2b_frontend/src/Components/HeroHome.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import '../Style/HeroHome.css'
+import React from 'react';
+import '../Style/HeroHome.css';
+import fondoHero from "../assets/fondoHero.png"
 function HeroHome() {
   return (
     <div>
-      <section class="hero">
-       <div class="hero-content">
-        <h1>Transforma el Bienestar de tu Equipo</h1>
-        <p>Mejora la productividad y felicidad de tus empleados con nuestra plataforma inteligente de bienestar laboral.</p>
-        <a href="/registroLogin" class="btn-primary">Empieza Ahora</a>
-       </div>
-      </section>
+      <section className="hero">
+  <img className="heroFondo" src={fondoHero} alt="Fondo hero"/>
+  <div className="hero-overlay"></div> {/* Overlay oscuro */}
+  <div className="hero-content">
+    <h1>Transforma el Bienestar de tu Equipo</h1>
+    <p className='descripcion1'>Mejora la productividad y felicidad de tus empleados con nuestra plataforma inteligente de bienestar laboral.</p>
+    <div className="hero-buttons">
+      <a href="/registroLogin" className="btn-primary">Empieza Ahora</a>
+    </div>
+    <div className="hero-stats">
+      <div>
+        <h3>+500</h3>
+        <p>Empresas Felices</p>
+      </div>
+      <div>
+        <h3>98%</h3>
+        <p>Mejora en la Satisfacción</p>
+      </div>
+    </div>
+  </div>
+</section>
 
     </div>
-  )
+  );
 }
 
-export default HeroHome
+export default HeroHome;
--- FrontEnd/b2b_frontend/src/Pages/Home.jsx
+++ FrontEnd/b2b_frontend/src/Pages/Home.jsx
@@ -2,16 +2,27 @@ import Navbar from "../Components/Navbar"
 import Inicio from "../Components/Inicio";
 import Resenas from "../Components/Resenas";
 import HeroHome from "../Components/HeroHome";
-
+import '../Style/Home.css'
 
 function Home() {
   return (
-    <div>
+  <>
+    <div className="navbarHome">
       <Navbar/>
-      <Inicio/>
+    </div>
+    
+    <div className="heroHome0">
       <HeroHome/>
+    </div>
+
+    <div className="inicioHome">
+      <Inicio/>
+    </div>
+
+    <div className="resenasHome">
       <Resenas/>
     </div>
+  </>
   )
 }
 
